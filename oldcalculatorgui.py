3d339d018599a98e197bc4c78636ddd01f2c9c1a
ddalki <nikkaanndreacruz1205@gmail.com>
2025-01-02 19:22:21 +0800

    finalized

--- oldcalculatorgui.py
+++ oldcalculatorgui.py
@@ -6,31 +6,39 @@ win.geometry(f"{(width := int(win.winfo_screenwidth() / 2.85))}x{(height := int(
 win.config(bg="#000000")
 win.wm_attributes('-transparentcolor')
 
-labels_info = [("Python GUI Basic Calculator v1", 160, 50, ("Century Gothic", 18)), ("First number", 20, 110, ("Century Gothic", 15)), ("Second number", 20, 150, ("Century Gothic", 15)), ("Result: ", 420, 110, ("Century Gothic", 15)), (" ", 420, 150, ("Century Gothic", 15))]
+labels_info = [("Python GUI Basic Calculator v1", 160, 50, ("Century Gothic", 18)), ("First number", 20, 110, ("Century Gothic", 15)), ("Second number", 20, 150, ("Century Gothic", 15)), ("Result: ", 420, 107, ("Century Gothic", 15)), (" ", 420, 148, ("Century Gothic", 15))]
 labels = []
 for (text, x, y, (font, size)) in labels_info:
     label = tk.Label(win, text=text, font=(font, size), fg="white", bg="black")
     label.place(x=x, y=y)
     labels.append(label)
 
-firstnum, secondnum = tk.Entry(win, font=("Century Gothic", 13), bg="black", fg="white"), tk.Entry(win, font=("Century Gothic", 13), bg="black", fg="white")
+firstnum, secondnum = tk.Entry(win, font=("Century Gothic", 13), bg="white", fg="black"), tk.Entry(win, font=("Century Gothic", 13), bg="white", fg="black")
 firstnum.place(x=200, y=110)
 secondnum.place(x=200, y=150)
 lblnresult = labels[4] 
+lblresult = labels[3]
 
 def add():
+    lblresult.config(text="Sum:")
     lblnresult.config(text=(int(firstnum.get()) + int(secondnum.get())), fg="green")
 def minus():
+    lblresult.config(text="Difference:")
     lblnresult.config(text=(int(firstnum.get()) - int(secondnum.get())), fg="green")
 def mult():
+    lblresult.config(text="Product:")
     lblnresult.config(text=(int(firstnum.get()) * int(secondnum.get())), fg="green")
 def div():
+    lblresult.config(text="Quotient:")
     lblnresult.config(text=(int(firstnum.get()) / int(secondnum.get())) if int(secondnum.get()) != 0 else "undefined", fg="green" if int(secondnum.get()) != 0 else "red")
 def floor():
+    lblresult.config(text="Floor Division:")
     lblnresult.config(text=(int(firstnum.get()) // int(secondnum.get())) if int(secondnum.get()) != 0 else "undefined", fg="green" if int(secondnum.get()) != 0 else "red")
 def mod():
+    lblresult.config(text="Remainder:")
     lblnresult.config(text=(int(firstnum.get()) % int(secondnum.get())) if int(secondnum.get()) != 0 else "undefined", fg="green" if int(secondnum.get()) != 0 else "red")
 def exp():
+    lblresult.config(text="Power:")
     lblnresult.config(text=(int(firstnum.get()) ** int(secondnum.get())), fg="green")
 
 button_frame = tk.Frame(win, bg="black")
